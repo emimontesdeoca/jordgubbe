ec5f8117c04a66849fe63a4a9743ecadeb4f7c46
Emi <emontesdeoc@gmail.com>
2017-04-13 05:18:40 +0200

    Adding files

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope, $http) {
+    $scope.items = []
+    $http.get('js/projects.json').then(function(response) {
+        $scope.items = response.data;
+    });
+
+    // Weather stuff
+
+    $(document).ready(function() {
+        loadWeather('Tenerife', '');
+    });
+
+    function loadWeather(location, woeid) {
+        $.simpleWeather({
+            location: location,
+            woeid: woeid,
+            unit: 'c',
+            success: function(weather) {
+                html = weather.city + ', ' + weather.region + ' - ' + weather.temp + '&deg;' + weather.units.temp;
+                $("#weather").html(html);
+            },
+            error: function(error) {
+                $("#weather").html('<p>' + error + '</p>');
+            }
+        });
+    }
+});
